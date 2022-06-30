7e547ffac7738f82a8e6b7cac002ac8068ca1131
sunil <mp17sunil97@gmail.com>
2022-06-30 18:10:09 +0530

    catorgy

--- js/script.js
+++ js/script.js
@@ -10,17 +10,54 @@ for(let i = 0; i < list.length; i++){
         this.classList.add('active');
 
         let dataFilter = this.getAttribute('data-filter');
+        // console.log(dataFilter);
 
-        for( let k = 0; k < itembox.length; k++){
+    
+        for( var k = 0; k < itembox.length; k++){
 
-            if(itembox[k].getAttribute('data-filter') == dataFilter || dataFilter == "all" ){
+            
+            if(itembox[k].getAttribute('data-filter') == dataFilter || dataFilter == "All" ){
+                console.log(itembox[k]);
+                itembox[k].classList.add('active'); 
                 itembox[k].classList.remove('hide');
-                itembox[k].classList.add('active');   
+                 
+            }else {
+                itembox[k].classList.remove('active');  
+                itembox[k].classList.add('hide');
             }
             
-            itembox[k].classList.remove('active');  
-            itembox[k].classList.add('hide');
+           
         }
 
     })
 }
+
+$('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: 0
+
+});
+
+// document.getElementById("menutoggleid").addEventListener("click", abc);
+
+// function abc() {
+//     document.getElementById("header").classList.toggle("open");
+// }
+
+// const divopen = document.getElementById('nav-menu');
+// const addBtn = document.getElementById('menutoggleid');
+
+
+// addBtn.addEventListener('click',()=>{
+
+//     if (divopen.classList.contains('open')) {
+//         divopen.classList.remove('open');
+//     } else {
+//         divopen.classList.add('open');
+//     }
+// })
+
+// preventdefault();
+
+
+
