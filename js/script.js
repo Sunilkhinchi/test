57252aa40c2d4069485e6a8473d2ad1ef2726e56
sunil <mp17sunil97@gmail.com>
2022-07-01 19:11:21 +0530

    last

--- js/script.js
+++ js/script.js
@@ -55,9 +55,19 @@ addBtn.addEventListener('click',()=>{
     } else {
         divopen.classList.add('open');
     }
-})
+});
 
-preventdefault();
+
+
+function disableScrolling(){
+    var x=window.scrollX;
+    var y=window.scrollY;
+    window.onscroll=function(){window.scrollTo(x, y);};
+}
+
+function enableScrolling(){
+    window.onscroll=function(){};
+}
 
 
 
